447751b99419922e78119173e17ede761c7d4f3a
Shrutika <mhatreshrutika5@gmail.com>
2024-05-25 00:20:45 +0530

    Initial commit

---
+++ apiAssignment/src/main/java/com/cashRich/apiAssignment/ApiAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.cashRich.apiAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAssignmentApplication.class, args);
+	}
+
+}
---
+++ apiAssignment/src/test/java/com/cashRich/apiAssignment/ApiAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashRich.apiAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
